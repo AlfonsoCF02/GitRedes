d3d8ca442614300bd989c3fb13eb018b8f65966c
Abraham Cordoba Perez <abcp2002@gmail.com>
2022-09-28 11:21:53 +0200

    kcf

--- P2/ejemploClienteChat.c
+++ P2/ejemploClienteChat.c
@@ -43,7 +43,7 @@ int main ( )
 	-------------------------------------------------------------------*/
 	sockname.sin_family = AF_INET;
 	sockname.sin_port = htons(2000);
-	sockname.sin_addr.s_addr =  inet_addr("127.0.0.1");
+	sockname.sin_addr.s_addr =  inet_addr("172.21.20.97");
 
 	/* ------------------------------------------------------------------
 		Se solicita la conexión con el servidor
